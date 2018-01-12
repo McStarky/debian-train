2409c647e5481a4967dd7063776660a27b16cc78
root <root@debian.nharng.blueteam.org>
2018-01-12 12:17:58 -0500

    fixed source list, removed color

--- debianconfig.sh
+++ debianconfig.sh
@@ -2,18 +2,19 @@
 #McStarky 2018
 
 #add repository for intel wifi drivers
-echo "# Debian 9 iwlwifi repo location"
+echo "# Debian 9 iwlwifi repo location" >> /etc/apt/sources.list
 echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" >> /etc/apt/sources.list
 
 #add firmware for drivers
-apt update && apt install firmware-iwlwifi pv sudo
+apt update && apt install firmware-iwlwifi -y
 
 #not sure what this does but do it
 modprobe -r iwlwifi ; modprobe iwlwifi
 
 apt upgrade -y
-#Proper colors ya boi
-setterm -term linux -back black -fore green
+
+#Proper colors edit: needs work only changes currently working shell
+#setterm -term linux -back black -fore green
 
 #this restarts the visual GUI, allowing wireless to appear in the option menu
 killall -HUP gnome-shell
